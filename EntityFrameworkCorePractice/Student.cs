571f725155ff3374cfe577ec1cbe9ccf943b6194
Cameron White <cameronwhite4121@gmail.com>
2024-07-25 14:55:30 -0700

    Made a student class to hold information

---
+++ EntityFrameworkCorePractice/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCorePractice
+{
+    public class Student
+    {
+        [Key] // Tell EF Core to make this a primary key (PK)
+        public int Id { get; set; }
+
+        public string FullName {  get; set; }
+
+        public DateOnly DateOfBirth {  get; set; }
+    }
+}
