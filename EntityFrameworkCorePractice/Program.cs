3e357d89db19807665125612a6aa7074d4d10a38
Cameron White <cameronwhite4121@gmail.com>
2024-07-25 17:24:11 -0700

    Created and added student objects to the db

--- EntityFrameworkCorePractice/Program.cs
+++ EntityFrameworkCorePractice/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.Write("Hello world");
+﻿using EntityFrameworkCorePractice;
+
+StudentContext dbContext = new StudentContext();
+
+Student s = new()
+{
+    FullName = "Cram man",
+    Email = "ilovecram@hotmail.com",
+    DateOfBirth = new DateOnly(2000,1,1)
+};
+
+dbContext.Students.Add(s);
+dbContext.SaveChanges();
+
+Student s2 = new()
+{
+    FullName = "Grooble",
+    Email = "theofficialgrooble@yahoo.com",
+    DateOfBirth = new DateOnly(1500, 1, 1)
+};
+
+dbContext.Students.Add(s2);
+dbContext.SaveChanges();
+
+List<Student> allStudents = dbContext.Students.ToList();
+
+foreach (Student stu in allStudents)
+{
+    Console.WriteLine($"{stu.FullName} has an email of {stu.Email}");
+    Console.WriteLine();
+}
